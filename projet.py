dfc2330292ab160db65e7e9d055167667771fec9
fedi-hmaidi <fedihmaidi12@gmail.com>
2025-07-09 18:38:04 +0100

    use squash to merge

--- projet.py
+++ projet.py
@@ -7,3 +7,9 @@ import math
 Data = [1,3,5]
 Prod = math.prod(Data)
 print('le produit est:', Prod)
+if Data:
+    print('le produit est:', Prod(Data))
+    print('le min est:', min(Data))
+    print('le produit est:', max(Data))
+else:
+    print('dossier vide')
